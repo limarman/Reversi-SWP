93629bfdfc52f09319f6b01d99cc8707bdc29fd5
Eric Remigius <eric.remigius@rwth-aachen.de>
2018-04-23 23:43:32 +0200

    Moved test files to a new folder

--- test/swpg3/TileTest.java
+++ test/swpg3/TileTest.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package swpg3Tests;
+package swpg3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
--- test/swpg3/Vector2iTest.java
+++ test/swpg3/Vector2iTest.java
@@ -1,4 +1,4 @@
-package swpg3Tests;
+package swpg3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
