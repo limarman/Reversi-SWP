77fdee836b2ec8632e67c1d07574a5fb2ac569eb
Ramil Sabirov <ramil.sabirov@rwth-aachen.de>
2018-06-20 17:03:15 +0200

    Modified Parameters

--- src/swpg3/ai/AI.java
+++ src/swpg3/ai/AI.java
@@ -45,7 +45,7 @@ public class AI {
 	public static double SC_TP_I;
 	
 	//Mobility parameter
-	public static double MOBILITY_BONUS = 15;
+	public static double MOBILITY_BONUS = 20;
 	
 	public static double M_SV;
 	public static double M_MV;
@@ -57,11 +57,11 @@ public class AI {
 	public static double M_ILF = 0.7;
 	
 	//OverrideStone paramaters
-	public static double OVERRIDE_BONUS = 220;
+	public static double OVERRIDE_BONUS = 250;
 	public static double OVERRIDE_IMPORTANCE = 1;
 	
 	//PositionalPlay parameters
-	public static double SOLID_SQUARE_BONUS = 6;
+	public static double SOLID_SQUARE_BONUS = 10;
 	public static double WEAK_SQUARE_BONUS = -5;
 	public static double BONUS_WEAK_SQUARE_BONUS = -3;
 	public static double CHOICE_WEAK_SQUARE_BONUS = -5;
