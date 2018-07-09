b341a9c0306e62a0720072e03b979fae7eba3f21
Ramil Sabirov <ramil.sabirov@rwth-aachen.de>
2018-07-09 21:28:01 +0200

    Removed debug messages.

--- src/swpg3/ai/calculator/PruningParanoidCalculator.java
+++ src/swpg3/ai/calculator/PruningParanoidCalculator.java
@@ -1,7 +1,6 @@
 package swpg3.ai.calculator;
 
 import java.util.HashSet;
-import java.util.LinkedList;
 
 import swpg3.ai.Clockmaster;
 import swpg3.ai.calculator.movesorter.BogoSorter;
@@ -121,7 +120,7 @@ public class PruningParanoidCalculator implements Calculator{
 		for(int i = sortedMoves.length-1; i>=0; i--) 
 		{
 			Map nextMap = map.clone();
-			Logger.log(LogLevel.ERROR, "Thinking on: " + sortedMoves[i]);
+//			Logger.log(LogLevel.ERROR, "Thinking on: " + sortedMoves[i]);
 			nextMap.applyMove(sortedMoves[i]);
 			byte nextPlayerNumber = (byte) (maxPlayerNumber % MapManager.getInstance().getNumberOfPlayers() + 1);
 			
@@ -238,7 +237,7 @@ public class PruningParanoidCalculator implements Calculator{
 		{
 			Map nextMap = map.clone();
 			nextMap.applyMove(sortedMoves[i]);
-			Logger.log(LogLevel.ERROR, "DEPTH: " + depth + " Calculate: " + sortedMoves[i]);
+//			Logger.log(LogLevel.ERROR, "DEPTH: " + depth + " Calculate: " + sortedMoves[i]);
 			byte nextPlayerNumber = (byte) (currentPlayerNumber % MapManager.getInstance().getNumberOfPlayers() + 1);
 			double value;
 			
@@ -351,7 +350,7 @@ public class PruningParanoidCalculator implements Calculator{
 		{
 			Map nextMap = map.clone();
 			nextMap.applyMove(sortedMoves[i]);
-			Logger.log(LogLevel.ERROR, "DEPTH: " + depth + " Calculate: " + sortedMoves[i]);
+//			Logger.log(LogLevel.ERROR, "DEPTH: " + depth + " Calculate: " + sortedMoves[i]);
 			byte nextPlayerNumber = (byte) (currentPlayerNumber % MapManager.getInstance().getNumberOfPlayers() + 1);
 			
 			double value = minPlayer(eval, maxPlayerNumber, nextPlayerNumber, depth-1, calcDeadLine, form, nextMap, 0, maxValue, beta);
