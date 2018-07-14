d39ccae9cd988e073b15e878626fe1b9374e1e2c
Ramil Sabirov <ramil.sabirov@rwth-aachen.de>
2018-07-14 17:35:06 +0200

    Integrated Blocks

--- src/swpg3/ai/evaluator/InversionaryEvaluator.java
+++ src/swpg3/ai/evaluator/InversionaryEvaluator.java
@@ -487,33 +487,34 @@ public class InversionaryEvaluator extends RelativeEvaluator implements Evaluato
 		
 		//analysis of the mobility
 		
-		//creating the counter for free possibleMoves
-		IntegerWrapper[] noPossibleMoves = new IntegerWrapper[MapManager.getInstance().getNumberOfPlayers()];
-		for(int i = 0; i<noPossibleMoves.length; i++) 
-		{
-			noPossibleMoves[i] = new IntegerWrapper();
-		}
-		
-		//creating the bitmaps of the map
-		BitMap[] bitmaps = new BitMap[MapManager.getInstance().getNumberOfPlayers()];
-		
-		for(int i = 0; i<bitmaps.length; i++) 
-		{
-			bitmaps[i] = new BitMap(MapManager.getInstance().getWidth(), MapManager.getInstance().getHeight());
-		}
-		
-		fillFreeMovesHorizontallyEastSide(map, noPossibleMoves, bitmaps);
-		fillFreeMovesHorizontallyWestSide(map, noPossibleMoves, bitmaps);
-		fillFreeMovesVerticallyNorthSide(map, noPossibleMoves, bitmaps);
-		fillFreeMovesVerticallySouthSide(map, noPossibleMoves, bitmaps);
-		fillFreeMovesDiagonallyNorthEastSide(map, noPossibleMoves, bitmaps);
-		fillFreeMovesDiagonallySouthWestSide(map, noPossibleMoves, bitmaps);
-		fillFreeMovesSemiDiagonallyNorthWestSide(map, noPossibleMoves, bitmaps);
-		fillFreeMovesSemiDiagonallySouthEastSide(map, noPossibleMoves, bitmaps);
+//		//creating the counter for free possibleMoves
+//		IntegerWrapper[] noPossibleMoves = new IntegerWrapper[MapManager.getInstance().getNumberOfPlayers()];
+//		for(int i = 0; i<noPossibleMoves.length; i++) 
+//		{
+//			noPossibleMoves[i] = new IntegerWrapper();
+//		}
+//		
+//		//creating the bitmaps of the map
+//		BitMap[] bitmaps = new BitMap[MapManager.getInstance().getNumberOfPlayers()];
+//		
+//		for(int i = 0; i<bitmaps.length; i++) 
+//		{
+//			bitmaps[i] = new BitMap(MapManager.getInstance().getWidth(), MapManager.getInstance().getHeight());
+//		}
+//		
+//		fillFreeMovesHorizontallyEastSide(map, noPossibleMoves, bitmaps);
+//		fillFreeMovesHorizontallyWestSide(map, noPossibleMoves, bitmaps);
+//		fillFreeMovesVerticallyNorthSide(map, noPossibleMoves, bitmaps);
+//		fillFreeMovesVerticallySouthSide(map, noPossibleMoves, bitmaps);
+//		fillFreeMovesDiagonallyNorthEastSide(map, noPossibleMoves, bitmaps);
+//		fillFreeMovesDiagonallySouthWestSide(map, noPossibleMoves, bitmaps);
+//		fillFreeMovesSemiDiagonallyNorthWestSide(map, noPossibleMoves, bitmaps);
+//		fillFreeMovesSemiDiagonallySouthEastSide(map, noPossibleMoves, bitmaps);
 				
-		for(int i = 0; i<noPossibleMoves.length; i++) 
+		for(int i = 0; i<MapManager.getInstance().getNumberOfPlayers(); i++) 
 		{
-			attributesPerPlayer[i][FREE_POS_MOVES] = noPossibleMoves[i].getValue();
+//			attributesPerPlayer[i][FREE_POS_MOVES] = noPossibleMoves[i].getValue();
+			attributesPerPlayer[i][FREE_POS_MOVES] = map.mobilityByBlocks(i+1);
 		}
 		
 		//finding out how many turns till own turn
