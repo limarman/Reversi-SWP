7a8c8879d10f56d21f906b30419f6879b467b394
Ramil Sabirov <ramil.sabirov@rwth-aachen.de>
2018-07-17 23:29:18 +0200

    Revert "Revert "Merge branch 'Development'""
    This reverts commit 9efd6c594f80693560cc6788c263c62ab3d19b01.

--- test/swpg3/ai/AnalyserTest.java
+++ test/swpg3/ai/AnalyserTest.java
@@ -2,6 +2,7 @@ package swpg3.ai;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
@@ -46,6 +47,232 @@ class AnalyserTest {
 		assertTrue(AI.solidSquares.get(0,3), "Missing solid tile at (0,3)");
 
 		assertEquals(14, AI.numberOfSolidSquares,"Solid Squares not correctly determined.");
+	}
+	
+	@Test
+	void testReachableSquaresNothingReachable()
+	{
+		String mapString = "2\n"
+				+ "0\n"
+				+ "0 1\n"
+				+ "3 3\n"
+				+ "0 0 0\n"
+				+ "0 1 0\n"
+				+ "0 0 0";
+		
+		MapManager mm = MapManager.getInstance();
+		mm.initializeMap(mapString);
+		Analyser.getInstance().analyseMap();
+		
+		for(int x = 0; x < 3; x++)
+		{
+			for(int y = 0; y < 3; y++)
+			{
+				assertFalse(AI.reachableSquares.get(x,y));
+			}
+		}
+	}
+	
+	@Test
+	void testReachableSquaresEverythingReachable()
+	{
+		String mapString = "2\n"
+				+ "0\n"
+				+ "0 1\n"
+				+ "3 3\n"
+				+ "0 0 0\n"
+				+ "0 1 2\n"
+				+ "0 0 0";
+		
+		MapManager mm = MapManager.getInstance();
+		mm.initializeMap(mapString);
+		Analyser.getInstance().analyseMap();
+		
+		for(int x = 0; x < 3; x++)
+		{
+			for(int y = 0; y < 3; y++)
+			{
+				assertTrue(AI.reachableSquares.get(x,y));
+			}
+		}
+	}
+	
+	@Test
+	void testReachableSquares2Components()
+	{
+		String mapString = "2\n"
+				+ "0\n"
+				+ "0 1\n"
+				+ "7 3\n"
+				+ "0 0 0\n"
+				+ "0 1 2\n"
+				+ "0 0 0\n"
+				+ "- - -\n"
+				+ "0 0 0\n"
+				+ "0 1 0\n"
+				+ "0 0 0";
+		
+		MapManager mm = MapManager.getInstance();
+		mm.initializeMap(mapString);
+		Analyser.getInstance().analyseMap();
+		
+		for(int x = 0; x < 3; x++)
+		{
+			for(int y = 0; y < 3; y++)
+			{
+				assertTrue(AI.reachableSquares.get(x,y));
+			}
+		}
+		
+		for(int x = 0; x < 3; x++)
+		{
+			for(int y = 3; y < 7; y++)
+			{
+				assertFalse(AI.reachableSquares.get(x,y));
+			}
+		}
+	}
+	
+	@Test
+	void testReachableSquares2ComponentsReachable()
+	{
+		String mapString = "2\n"
+				+ "0\n"
+				+ "0 1\n"
+				+ "7 3\n"
+				+ "0 0 0\n"
+				+ "0 1 2\n"
+				+ "0 0 0\n"
+				+ "- - -\n"
+				+ "2 0 0\n"
+				+ "0 1 0\n"
+				+ "0 0 0";
+		
+		MapManager mm = MapManager.getInstance();
+		mm.initializeMap(mapString);
+		Analyser.getInstance().analyseMap();
+		
+		for(int x = 0; x < 3; x++)
+		{
+			for(int y = 0; y < 3; y++)
+			{
+				assertTrue(AI.reachableSquares.get(x,y));
+			}
+		}
+		
+		for(int x = 0; x < 3; x++)
+		{
+			for(int y = 4; y < 7; y++)
+			{
+				assertTrue(AI.reachableSquares.get(x,y));
+			}
+		}
+	}
+	
+	@Test
+	void testReachableSquares2ComponentsTransition()
+	{
+		String mapString = "2\n"
+				+ "0\n"
+				+ "0 1\n"
+				+ "7 3\n"
+				+ "0 0 0\n"
+				+ "0 1 2\n"
+				+ "0 0 0\n"
+				+ "- - -\n"
+				+ "0 0 0\n"
+				+ "0 1 0\n"
+				+ "0 0 0\n"
+				+ "1 2 4 <-> 1 4 0";
+		
+		MapManager mm = MapManager.getInstance();
+		mm.initializeMap(mapString);
+		Analyser.getInstance().analyseMap();
+		
+		for(int x = 0; x < 3; x++)
+		{
+			for(int y = 0; y < 3; y++)
+			{
+				assertTrue(AI.reachableSquares.get(x,y));
+			}
+		}
+		
+		for(int x = 0; x < 3; x++)
+		{
+			assertFalse(AI.reachableSquares.get(x,3));
+		}
+		
+		for(int x = 0; x < 3; x++)
+		{
+			for(int y = 4; y < 7; y++)
+			{
+				assertTrue(AI.reachableSquares.get(x,y));
+			}
+		}
+	}
+	
+	@Test
+	void testReachableSquaresCompetitionMap()
+	{
+		
+		String mapString = "4\n" + 
+				"1\n" + 
+				"0 0\n" + 
+				"27 24\n" + 
+				"- - - - - - - - - - - - - - - - - - - - - - - -\n" + 
+				"- - - - - - - - b b - - - - - - b x - - - - - -\n" + 
+				"- - - - - - - - x b - - - - - - b b - - - - - -\n" + 
+				"- - - - - - - - - - - - - - - - - - - - - - - -\n" + 
+				"- - - - - - - - - - 1 0 0 0 0 2 - - - - - - - -\n" + 
+				"- - - - - - - - - - - - 4 0 0 0 - - - - - - - -\n" + 
+				"- - - - - - - - - - - - - i 3 - - - - - - - - -\n" + 
+				"- - - - - - - - - - - - - - - - - - - - - - - -\n" + 
+				"- - - - b x 0 0 0 0 0 - - 0 0 0 0 0 x b - - - -\n" + 
+				"- - - b x x 0 0 0 0 0 - - 0 0 0 0 0 x x b - - -\n" + 
+				"- - b x x 0 0 0 0 0 - - - - 0 0 0 0 0 x x b - -\n" + 
+				"- - x x 0 0 0 0 0 0 0 - - 0 0 0 0 0 0 0 x x - -\n" + 
+				"- - 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 - -\n" + 
+				"- - 0 0 0 0 0 0 0 0 1 2 3 4 0 0 0 0 0 0 0 0 - -\n" + 
+				"- - 0 0 0 0 0 0 0 0 2 3 4 1 0 0 0 0 0 0 0 0 - -\n" + 
+				"- - 0 0 0 0 0 0 0 0 3 4 1 2 0 0 0 0 0 0 0 0 - -\n" + 
+				"- - 0 0 0 0 0 0 0 0 4 1 2 3 0 0 0 0 0 0 0 0 - -\n" + 
+				"- - - 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 - - -\n" + 
+				"- - - - 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 - - - -\n" + 
+				"- - - - - - - 0 0 0 0 0 0 0 0 0 0 - - - - - - -\n" + 
+				"- - - - - - - - - - 0 0 0 0 - - - - - - - - - -\n" + 
+				"- - - - - - - - - - b 0 0 b - - - - - - - - - -\n" + 
+				"- - - - - - - - - - 0 0 0 0 - - - - - - - - - -\n" + 
+				"- - - - - - - - - c 0 0 0 0 c - - - - - - - - -\n" + 
+				"- - - - - - - - b 0 0 0 0 0 0 b - - - - - - - -\n" + 
+				"- - - - - - - 0 0 0 0 0 0 0 0 0 0 - - - - - - -\n" + 
+				"- - - - - - - 0 0 0 0 0 0 0 0 0 0 - - - - - - -";
+		
+		MapManager mm = MapManager.getInstance();
+		mm.initializeMap(mapString);
+		Analyser.getInstance().analyseMap();
+		
+		// Eyes
+		assertFalse(AI.reachableSquares.get(8, 1));
+		assertFalse(AI.reachableSquares.get(8, 2));
+		assertFalse(AI.reachableSquares.get(9, 1));
+		assertFalse(AI.reachableSquares.get(9, 2));
+		
+		assertFalse(AI.reachableSquares.get(16, 1));
+		assertFalse(AI.reachableSquares.get(16, 2));
+		assertFalse(AI.reachableSquares.get(17, 1));
+		assertFalse(AI.reachableSquares.get(17, 2));
+		//Mouth
+		assertFalse(AI.reachableSquares.get(10, 4));
+		assertFalse(AI.reachableSquares.get(11, 4));
+		assertFalse(AI.reachableSquares.get(12, 4));
+		assertFalse(AI.reachableSquares.get(13, 4));
+		assertFalse(AI.reachableSquares.get(14, 4));
+		assertFalse(AI.reachableSquares.get(15, 4));
+		//Trophy
+		assertTrue(AI.reachableSquares.get(15, 10));
+		
+		// To view the BitMap
+		// AI.reachableSquares.print();
 	}
 
 }
